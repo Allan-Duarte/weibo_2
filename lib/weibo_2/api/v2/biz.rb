3572ed57ab8a810abe375a477fdfcfa415ce8e47
Ivan Ramirez <ivan.ramirez@gmail.com>
2016-11-11 12:36:58 -0500

    Add missing method to load status.

--- lib/weibo_2/api/v2/biz.rb
+++ lib/weibo_2/api/v2/biz.rb
@@ -8,15 +8,23 @@ module WeiboOAuth2
           hashie get("statuses/user_timeline_batch.json", :params => opt)
         end
 
+        # https://c.api.weibo.com/2/statuses/show_batch/biz.json
+        def show_status(id)
+          hashie get("statuses/show_batch/biz.json", :params => opt)
+        end
+
         #write interfaces
+        # https://c.api.weibo.com/2/friendships/create/biz.json
         def create_friendships(opt={})
           hashie post("friendships/create/biz.json", :params => opt)
         end
 
+        # https://c.api.weibo.com/2/statuses/repost/biz.json
         def repost(id, opt={})
           hashie post("statuses/repost/biz.json", :params => {"id" => id}.merge(opt))
         end
 
+        # https://c.api.weibo.com/2/comments/create/biz.json
         def create_comment(comment, id, opt={})
           hashie post("comments/create/biz.json", :params => {:comment => comment, :id => id}.merge(opt))
         end
